eab7b5e3988494a5152f2a07d398b17d2384ccd8
thekewlstore <thekewlstore@gmail.com>
2025-01-29 22:03:15 -0500

    Working addition of a custom sprite

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -725,12 +725,16 @@ const Dex = new class implements ModdedDex {
 			// @ts-ignore
 			id = toID(pokemon.volatiles.formechange[1]);
 		}
-		let num = this.getPokemonIconNum(id, pokemon?.gender === 'F', facingLeft);
+		// let num = this.getPokemonIconNum(id, pokemon?.gender === 'F', facingLeft);
 
-		let top = Math.floor(num / 12) * 30;
-		let left = (num % 12) * 40;
+		// let top = Math.floor(num / 12) * 30;
+		// let left = (num % 12) * 40;
+		if(id.includes('silvally')){
+			id = toID('silvally');
+		}
 		let fainted = ((pokemon as Pokemon | ServerPokemon)?.fainted ? `;opacity:.3;filter:grayscale(100%) brightness(.5)` : ``);
-		return `background:transparent url(${Dex.resourcePrefix}sprites/pokemonicons-sheet.png?v17) no-repeat scroll -${left}px -${top}px${fainted}`;
+
+		return `background:transparent url(${Dex.resourcePrefix}sprites/pokemon_icons/${id}_icon.png) no-repeat scroll ${fainted}`;
 	}
 
 	getTeambuilderSpriteData(pokemon: any, gen: number = 0): TeambuilderSpriteData {
